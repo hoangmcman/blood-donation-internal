b26d2f2d4fc5527163ce004fc52c258633c60d90
Nguyen Minh Hoang <hoang390dbp@gmail.com>
2025-06-30 21:25:42 +0700

    Sửa lỗi

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,9 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
-
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import vercel from 'vite-plugin-vercel';
+ 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-    extensions: [".js", ".ts", ".jsx", ".tsx", ".json"],
-  },
   server: {
-    port: 6173,
+    port: process.env.PORT as unknown as number,
   },
-  "base": "/",
-})
+  plugins: [vercel()],
+});
