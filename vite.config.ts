5c18cecf38d73b2a525b1e493e8d70a4bf07a85f
Nguyen Minh Hoang <hoang390dbp@gmail.com>
2025-06-30 22:19:55 +0700

    Sửa lỗi

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
-import { defineConfig } from 'vite';
-import vercel from 'vite-plugin-vercel';
- 
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import vercel from 'vite-plugin-vercel'
+
 export default defineConfig({
+  plugins: [react(), vercel()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    }
+  },
   server: {
     port: process.env.PORT as unknown as number,
   },
-  plugins: [vercel()],
-});
+})
